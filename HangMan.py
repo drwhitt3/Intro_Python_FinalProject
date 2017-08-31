454cdedcb385686380efecd28e2bf527767149d2
Daniel Whitt <drwhitt3@gmail.com>
2017-08-30 17:02:23 -0700

    added in docstrings and comments

--- HangMan.py
+++ HangMan.py
@@ -1,36 +1,59 @@
+#!/usr/bin/env python
+'''Final Project
+Daniel Whitt
+8/30/2017
+
+These functions allow us to play the game Hangman
+'''
+
 import random
 import string
 
 class HangMan():
     def __init__(self):
+        #this just initializes the class HangMan and does not take any arguments
         self.self = self
 
     def wordList(self,word1,word2):
+        #this creates the possible words that have to be guessed
+        #it takes in two arguments
         words = ["Jon", "Tyrion", "Sansa", "Arya"]
         words.extend([word1,word2])
         word = random.choice(words).upper()
+        #a word is randomly picked and then returned as the output of this function
         return word
 
     def wordCheck(self, word, guesses, guess):
-        status = " "
+        #this function takes the word that was selected, the dictionary of guesses, and the user's guess
+        #it will check the guess against the actual word and return the letters of the word that have been guessed
+        word_status = " "
         i = 0
         matches = 0
         for letter in word:
             if guesses[letter]:
-                status += letter
+                word_status = word_status + letter
             else:
-                status += "*"
+                word_status = word_status + "*"
             if letter == guess:
-                matches += 1
+                matches = matches + 1
 
         if matches >= 1:
             print("Yes :) The word contains a match for: ", guess)
         else:
             print("Sorry :( The word does not contain a match for: ", guess)
 
-        return status
+        #It will print out if there was a match or not
+        #It also returns the status of the unknown word after the guess
+        #It will show letters that have been guessed and it will hide letters that have not been guessed
+        return word_status
 
     def main(self,word):
+        #This is the main function that actually runs HangMan
+        #The input is the random word that was selected
+        #It will ask the user for their guess and it will return how many matches there, if any
+        #If there were matches, it will show the word with those letters revealed
+        #Once the word is guessed correctly, it will display how many attempts it took along with writing
+        #the word to a .txt file
         test = HangMan()
         guesses = dict.fromkeys(string.ascii_uppercase, False)
         guessed = False
--- Main.py
+++ Main.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python
+'''Final Project
+Daniel Whitt
+8/30/2017
+
+This runs the HangMan.py
+'''
+
 import HangMan
 import sys
 
 test = HangMan.HangMan()
+#instantiates HangMan
 
 
 try:
     if __name__ == "__main__":
         get_word = test.wordList(sys.argv[1],sys.argv[2])
         test.main(get_word)
+        #this allows us to get input from the command line
 except IndexError:
     print("Please only enter two extra words")
+    #if we get an IndexError, we instead print the above statement
